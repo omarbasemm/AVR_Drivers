15cdcdef8ea11b6bed48b61885176d45ccc63df1
omarbasemm <110375875+omarbasemm@users.noreply.github.com>
2024-12-12 03:24:17 +0200

    Add files via upload

---
+++ MCAL/StdTypes.h
@@ -0,0 +1,28 @@
+/*
+ * StdTypes.h
+ * For Genaral & Student Use only
+ * Created: 12/11/2024 10:20:14 PM
+ *  Author: Omar Basem Abdulsalam
+ */
+
+
+
+#ifndef STDTYPES_H_
+#define STDTYPES_H_
+
+typedef signed char			s8;
+typedef unsigned char		u8;
+
+typedef signed short int	s16;
+typedef unsigned short int	u16;
+
+typedef signed long int		s32;
+typedef unsigned long int	u32;
+
+typedef float				f32;
+typedef double				f64;
+
+	
+
+
+#endif /* STDTYPES_H_ */
